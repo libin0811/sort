97001a8f98c0bcd3b691f9a65559caf91219bce9
unknown <511378710@qq.com>
2018-11-25 09:13:30 +0800

    add select_sort and let the algorithm be the function

--- sort/buble_sort/main.cpp
+++ sort/buble_sort/main.cpp
@@ -3,22 +3,30 @@
 using namespace std;
 int main()
 {
-	int N,temp;
-	bool flag;
+	int N, temp;
 	void myQuick_sort(vector<int>&vec, int low, int high);
 	int parition(vector<int>&vec, int low, int high);
-	void swap(int &,int &);
+	void select_sort(vector<int>&vec);
+	void swap(int &, int &);
 	cout << "请输入要排序数据的长度：" << endl;
 	cin >> N;
 	vector<int>vec;
 	cout << "请依次输入排序数据：" << endl;
-	for (int i = 0; i < N;i++)
+	for (int i = 0; i < N; i++)
 	{
 		cin >> temp;
 		vec.push_back(temp);
 	}
-/*	
-/*冒泡排序
+	//	myQuick_sort(vec, 0, N - 1);
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << " ";
+	}
+	return 0;
+}
+/*冒泡排序*/
+void buble_sort(vector<int>vec){
+    bool flag;
 	for (int i = 0; i < vec.size() - 1; i++)
 	{
 		flag = false;
@@ -33,15 +41,22 @@ int main()
 			if (flag == false)
 				break;
 	}
-	*/
-/*快速排序*/
-	myQuick_sort(vec, 0, N - 1);
-	for (int i = 0; i < vec.size(); i++)
-	{
-		cout << vec[i] << " ";
 	}
-	return 0;
-}
+	/**/
+/*快速排序*/
+	/*选择排序*/
+	void select_sort(vector<int>&vec)
+	{
+		 int temp=65535;
+		 for (int j = 0; j < vec.size(); j++){
+			 for (int i = j+1; i < vec.size()-j; i++)
+			 {
+				 if (vec[i] < temp)
+					 temp = vec[i];
+			 }
+			 swap(vec[j],temp);
+		 }
+	}
 	void swap(int &a, int &b)
 	{
 		int temp;
